2fc15ee33a1f1dd3d3d68458e733465ae7834bca
Harshkaushik04 <142307211+Harshkaushik04@users.noreply.github.com>
2024-06-14 05:23:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import copy
 
-from useful_functions import gene_to_unprocessed_data,unprocessed_data_to_processed_data,processed_data_to_brain,graph_visuals,genes_hexa_dict_generator,co_ordinates_initialize_dict,genes_to_brain,directions_dict_generator,sense_to_inputs_activations,update_activations,position_errors_resolve,change,back_to_normal,mutate,replicate,condition,check
+from useful_functions import gene_to_unprocessed_data,unprocessed_data_to_processed_data,processed_data_to_brain,graph_visuals,genes_hexa_dict_generator,co_ordinates_initialize_dict,genes_to_brain,directions_dict_generator,sense_to_inputs_activations,update_activations,position_errors_resolve,change,back_to_normal,mutate,replicate,condition,check,genes_hexa_list_to_genes_list
 from pygame_env import player,env,circle,playerencoder
 from hashing_encoding_colors import encode_players_with_same_genes
 import pygame
@@ -9,11 +9,10 @@ import json
 # logging.basicConfig(level=logging.INFO)
 # logger = logging.getLogger(__name__)
 def main():
-    player_size=4
-    size_parameter=6
+    size_parameter=5
     num_neurons=[11,2,6] # [sensory_neurons,internal_neurons,output_neurons]
-    env_width=600
-    env_height=600
+    env_width=500
+    env_height=500
     population=300
     default_population=300
     steps_per_gen=120
@@ -29,7 +28,7 @@ def main():
             co_ordinates_dict,rect_co_ordinates_dict=co_ordinates_initialize_dict(env_width,env_height,population,size_parameter)
             #print(f'co_ordinates_dict:{co_ordinates_dict}')
             directions_dict=directions_dict_generator(population)
-            colors_dict=encode_players_with_same_genes(genes_hexa_dict)
+            colors_dict,max_gene,max_value=encode_players_with_same_genes(genes_hexa_dict)
             activation_inputs_dict={}
             players_dict={}
             for i in range(num_neurons[0]): #for unused brain input activations
@@ -47,12 +46,19 @@ def main():
                 #print('unused_brain.genes_list:',unused_brain.genes_list)
                 co_ordinates_list=co_ordinates_dict['player'+str(i)]
                 rect_co_ordinates_list=rect_co_ordinates_dict['player'+str(i)]
+                #print(rect_co_ordinates_list)
                 direction=directions_dict['player'+str(i)]
                 color=colors_dict['player'+str(i)]
-                pygame_object=circle(co_ordinates_list,color,radius=player_size//2)
-                players_dict['player'+str(i)]=player(co_ordinates_list,unused_brain.genes_list,unused_brain,pygame_object,direction,[co_ordinates_list],rect_co_ordinates_list)
+                pygame_object=circle(co_ordinates_list,color,radius=size_parameter//2)
+                co_ordinates_history_list=[copy.deepcopy(co_ordinates_list)]
+                players_dict['player'+str(i)]=player(co_ordinates_list,unused_brain.genes_list,unused_brain,pygame_object,direction,co_ordinates_history_list,rect_co_ordinates_list)
+                #print(players_dict['player'+str(i)].rect_co_ordinates_list)
                 #print('players_dict[player+str(i)].brain.genes_list:',players_dict['player'+str(i)].brain.genes_list)
         for step in range(steps_per_gen):
+            # for i in range(len(list(players_dict.keys()))):
+                # if step%2==0:
+                #     print(f'player{i} co_ordinates history::',players_dict['player'+str(i)].co_ordinates_history_list)
+                #     print(f'player{i} rect co ordinates:', players_dict['player' + str(i)].co_ordinates_history_list)
             print("step:",step)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -66,24 +72,40 @@ def main():
             players_dict,stored_neurons_dict,stored_incoming_exhausted_dict=update_activations(players_dict,combined_activation_inputs_dict)
             players_dict=change(players_dict, screen, size_parameter, kill_neuron=False)
             players_dict=back_to_normal(players_dict,stored_neurons_dict,stored_incoming_exhausted_dict)
-            pygame.image.save(screen, rf'D:\New_Evolution_16_genome_length_images\generation{generation+1}\image{step+generation*steps_per_gen+1}.png')
+            pygame.image.save(screen, rf'D:\New_Evolution_improved_12_genome_length_images\generation{generation+1}\image{step+generation*steps_per_gen+1}.png')
         players_dict,remaining_players_list=condition(players_dict,screen,inp=2)
         remaining_players_number.append(len(remaining_players_list))
         dicti = {}
+        another_dicti={}
         for i in range(len(remaining_players_list)):
             dicti[f'player{i}_genes_list'] = players_dict[remaining_players_list[i]].genes_list
-        players_json = json.dumps(dicti, cls=playerencoder)
+            another_dicti[f'player{i}_genes_hexa_list']=players_dict[remaining_players_list[i]].brain.genes_hexa_list
+        players_json = json.dumps(dicti)
+        players_another_json=json.dumps(another_dicti)
+        list_json=json.dumps(remaining_players_number)
+        max_gene_json=json.dumps(max_gene)
         with open('another_testing_file.py', 'w') as f:
             f.write('players_genes_list=' + players_json)
             f.write('\n')
-            f.write('remaining_players_number=[')
-            for i in range(len(remaining_players_number)):
-                f.write(str(remaining_players_number[i])+',')
-            f.write(']')
+            f.write('players_genes_hexa_list='+players_another_json)
+            f.write('\n')
+            f.write('remaining_players_number='+list_json)
+            f.write('\n')
+            f.write('num_neurons=[11,2,6]')
+            f.write('\n')
+            f.write('max_gene='+max_gene_json)
+            f.write('\n')
+            f.write('max_number_of_same_genes='+str(max_value))
+            f.write('\n')
+            f.write('from useful_functions import graph_visuals,genes_hexa_list_to_genes_list')
+            f.write('\n')
+            f.write('max_genes_list=genes_hexa_list_to_genes_list(max_gene,num_neurons)')
+            f.write('\n')
+            f.write('graph_visuals(max_genes_list)')
         players_dict,num_mutations=mutate(players_dict,num_neurons,remaining_players_list,mutation_prob=0.002)
         print('num_mutations:',num_mutations)
         print('num_players_after_mutation:',len(players_dict))
-        players_dict,population=replicate(players_dict,env_width,env_height,size_parameter,remaining_players_list,player_size,default_population=default_population)
+        players_dict,population,max_gene,max_value=replicate(players_dict,env_width,env_height,size_parameter,remaining_players_list,default_population=default_population)
         print('num_players_after_replication:',default_population,' or ',len(players_dict))
     return players_dict,remaining_players_number
 
--- make_folders.py
+++ make_folders.py
@@ -1,4 +1,4 @@
-import os
-os.makedirs(rf"D:\New_Evolution_16_genome_length_images")
-for i in range(100):
-    os.makedirs(rf'D:\New_Evolution_16_genome_length_images\generation{i+1}')
+# import os
+# os.makedirs(rf"D:\New_Evolution_improved_12_genome_length_images")
+# for i in range(100):
+#     os.makedirs(rf'D:\New_Evolution_improved_12_genome_length_images\generation{i+1}')
